c02657a917fd0e3f42eb0cb5ac7dc7342c09c874
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2024-01-28 18:37:41 +0800

    fix mac build
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

--- env_settings.cmake
+++ env_settings.cmake
@@ -1,4 +1,22 @@
 add_definitions(-DSTEAM_RELEASE=1)
 
-set(VCPKG_ROOT C:/source/vcpkg)
-set(QT_ROOT D:/Qt/6.6.1/msvc2019_64)
+## Template
+if (WIN32)
+
+elseif (APPLE)
+
+elseif (UNIX)
+
+endif ()
+## Template
+
+
+if (WIN32)
+    set(VCPKG_ROOT C:/source/vcpkg)
+    set(QT_ROOT D:/Qt/6.6.1/msvc2019_64)
+elseif (APPLE)
+    set(VCPKG_ROOT /Users/shark/source/vcpkg)
+    set(QT_ROOT /Users/shark/Qt/6.4.3/macos)
+elseif (UNIX)
+
+endif ()
